c811f7bb1a36a29cb9b83ed4d39cb9dd4b2c6a6a
Johansel <johanselp24@gmail.com>
2022-07-01 09:01:40 -0400

    change the name app

--- odontar-art/capacitor.config.ts
+++ odontar-art/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
  // appName: 'odontar-art',
- appName: 'Odonto-Art',
+ appName: 'odontar-art',
 
   webDir: 'www',
   bundledWebRuntime: false
