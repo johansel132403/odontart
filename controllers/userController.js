b4e6e6156fdb213ce8298056313060f2f51f3bd1
Johansel <johanselp24@gmail.com>
2022-07-07 11:46:23 -0400

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -997,15 +997,19 @@ async function updateAllNote(req,res, next){
         
                
             
-            await  User.updateMany({},paramsBody,{multi: true});
+         let f =   await  User.updateMany({},paramsBody,{multi: true});
          //   req.setTimeout((4 * 60 * 1000) + 1);
             // req.socket.removeAllListeners('timeout'); // This is the work around
             // req.socket.once('timeout', () => {
             //     req.timedout = true;
             //     res.status(504).send('Timeout');
             // });
+
+            if(f){
+                next()  
+
+            }
         
-            // next()  
        
         
     
