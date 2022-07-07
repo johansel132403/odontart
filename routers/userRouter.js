e840901e3863982ea6a40289db5943ff1d6d434c
Johansel <johanselp24@gmail.com>
2022-07-07 11:32:39 -0400

    .

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -41,7 +41,7 @@ api.delete('/borrarnota/:id',controllers.deleteNote);
 api.post('/imagennota/:id',controllers.imagenNote);
 api.get('/getAllNote',controllers.obtenerTodasNotificacion);
 api.get('/getOneNote',controllers.obtenerUnoNota);
-api.put('/updateNoteView',[timeout('25s')],controllers.updateAllNote);  
+api.put('/updateNoteView',controllers.updateAllNote);  
 api.put('/updateAllNoteWithId/:id',md_auth.ensureAuth,controllers.updateAllNoteWithId);
 api.get('/getImagenChat/:email',controllers.getImgFromChat);
 
