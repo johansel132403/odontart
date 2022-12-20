95890af8b1c7dbd7e6911e699c089ce1211a06ac
Johansel <johanselp24@gmail.com>
2022-12-20 15:11:46 -0400

    changing herokus

--- odontar-art/src/app/services/global.ts
+++ odontar-art/src/app/services/global.ts
@@ -1,7 +1,12 @@
 export var Global = {
     // url:'http://localhost:3000/api/'
     // url:'http://ac-ccswtcz-shard-00-02.tvfjxum.mongodb.net/api/'
-     url:'https://odontoart.herokuapp.com/api/'
+   
+
+    // esta es la url del servidor de herokuapp......
+   // url:'https://odontoart.herokuapp.com/api/'
+
+   url:'https://odontart-production.up.railway.app/api/'
 
    
    
